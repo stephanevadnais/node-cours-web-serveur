04bd09ed73bdd7f3ba4f15d0bff55c310cac06cb
Vladsteak <34750709+Vladsteak@users.noreply.github.com>
2019-03-17 11:59:02 -0400

    Correction URL - URI MongooseConfigurationConnection

--- serveur/db/mongooseConfigurationConnection.js
+++ serveur/db/mongooseConfigurationConnection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/Abonnement',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Abonnement',{ useNewUrlParser: true });
 
 
 module.exports = {mongoose};
